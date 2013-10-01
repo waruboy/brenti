7a1ee13f3159b854f2931200a2a3deca65e6c8d0
Taufiq Muhammadi <waruboy@yahoo.com>
2013-10-02 05:02:29 +0700

    basic layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,40 +2,25 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	subject { page }
+
 	describe "Home page" do
-
-		it "should have the content 'Brenti'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Brenti')
-		end
-
-		it "should have title 'Depan'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Depan - Brenti")
-		end
+		before { visit root_path }
+		it { should  have_content('Brenti') }
+		it { should  have_title("Depan - Brenti") }
 	end
 
 	describe "Help page" do
 
-		before {visit '/static_pages/help'}
-		it "should have the content 'Petunjuk'" do
-			expect(page).to have_content('Petunjuk')
-		end
-
-		it "should have title 'Depan'" do
-			expect(page).to have_title("Petunjuk - Brenti")
-		end
+		before { visit help_path }
+		it { should have_content('Petunjuk') }
+		it { should have_title("Petunjuk - Brenti") }
 	end
 
 	describe "Contact page" do
 
-		before {visit '/static_pages/contact'}
-		it "should have the content 'Hubungi'" do
-			expect(page).to have_content('Hubungi')
-		end
-
-		it "should have title 'Kontak'" do
-			expect(page).to have_title("Kontak - Brenti")
-		end
+		before { visit contact_path }
+		it { should have_content('Hubungi') }
+		it { should have_title("Kontak - Brenti") }
 	end
 end
