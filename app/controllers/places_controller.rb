ae77e4e0abb362532ffa755e5791f460abe5300e
Taufiq Muhammadi <waruboy@yahoo.com>
2013-10-15 13:57:43 +0700

    halaman depan

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,32 @@ class PlacesController < ApplicationController
 	def show
 		@place = Place.find(params[:id])
 	end
+	
+	def new
+		@place = Place.new
+		@location = Location.new
+		@haltes = Halte.all
+	end
+
+	def create
+		@place = Place.new(place_params)
+		@location = Location.new(location_params)
+		@place.locations << @location
+		if @place.save
+		else
+			@haltes = Halte.all
+			render 'new'
+		end
+	end
+
+
+
+	private
+		def location_params
+			params.require(:location).permit(:halte_id)
+		end
+
+		def place_params
+			params.require(:place).permit(:name, :description)
+		end
 end
--- spec/requests/place_pages_spec.rb
+++ spec/requests/place_pages_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe "PlacePages" do
 
 	subject { page }
-	let(:koridor) { FactoryGirl.create(:koridor) }
-	let(:halte) { FactoryGirl.create(:halte, koridor: koridor) }
-	let(:destinasi) { FactoryGirl.create(:place) }
+	let!(:koridor) { FactoryGirl.create(:koridor) }
+	let!(:halte) { FactoryGirl.create(:halte, koridor: koridor) }
+	let!(:destinasi) { FactoryGirl.create(:place) }
 
 	describe "detail page" do
 		before { visit place_path(destinasi) }
@@ -13,4 +13,24 @@ describe "PlacePages" do
 		it { should have_title(destinasi.name) }
 	end
 
+	describe "create new place" do
+		before { visit new_place_path }
+		let(:submit) { "Daftarkan destinasi" }
+
+		describe "with invalid information" do
+			it "should not create a place" do
+				expect { click_button submit }.not_to change(Place, :count)
+			end
+		end
+
+		# describe "with valid information" do
+		# 	before do
+		# 		fill_in "Name", 	with: "Pasar Festival"
+		# 		select "Gor Sumantri", from: "location[halte_id]"
+		# 	end
+		# 	it "should create a place" do
+		# 		expect { click_button submit }.to change(Place, :count).by(1)
+		# 	end
+		# end
+	end
 end
