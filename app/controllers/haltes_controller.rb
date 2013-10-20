56f5548db133e75d75d51061da22a2a6f7b2ffd8
Taufiq Muhammadi <waruboy@yahoo.com>
2013-10-20 09:41:59 +0700

    simple halte search

--- app/controllers/haltes_controller.rb
+++ app/controllers/haltes_controller.rb
@@ -4,4 +4,9 @@ class HaltesController < ApplicationController
 		@halte = Halte.find(params[:id])
 		@places = @halte.places
 	end
+
+	def index
+		halte = Halte.search(params[:search]).first
+		redirect_to halte_path(halte)
+	end
 end
--- app/models/halte.rb
+++ app/models/halte.rb
@@ -3,4 +3,12 @@ class Halte < ActiveRecord::Base
 	has_many :locations, dependent: :destroy
 	has_many :places, through: :locations
 
+
+	def self.search(search)
+		if search
+			find(:all, conditions: ['nama LIKE ?', "%#{search}%"])
+		else
+			find(:all)
+		end
+	end
 end
